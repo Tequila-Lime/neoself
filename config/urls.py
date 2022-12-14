34621aaf839aacf0877db66183011b6db04cf7fd
Aaron Stockdale <shadowstock76@gmail.com>
2022-12-14 10:09:02 -0500

    Toekn authentification works

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('registration.backends.simple.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('',include('neoself.urls'))
+    path('',include('neoself.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
