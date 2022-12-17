e740f2468c0f12337488d1355e85d516e3d0a4f9
Aaron Stockdale <shadowstock76@gmail.com>
2022-12-17 11:08:20 -0500

    Adding to documentations

--- neoself/urls.py
+++ neoself/urls.py
@@ -6,8 +6,6 @@ from django.conf import settings
 urlpatterns = [
     path('user/', views.UserView.as_view(), name='user-profile'),
     path('user/self/', views.UserDetail.as_view(), name='user-detail'),
-    path('user/search/', views.UserSearchList.as_view(), name="user-search"),
-    path('auth/users/me/avatar/', views.UserAvatarView.as_view(), name='user_avatar'),
     path('questionnaire/', views.QuestionnaireView.as_view(), name='habit-questionnaire'),
     path('questionnaire/<int:pk>/',views.QuestionnaireDetail.as_view(), name='specific-habit-questionnaire'),
     path('reflection/', views.ReflectionView.as_view(), name="reflection"),
