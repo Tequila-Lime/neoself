99f19b5a73eec374d985b88007b304d517531be0
Your Name <alobryan2@gmail.com>
2023-01-10 15:39:20 -0500

    small mistake fix

--- neoself/views.py
+++ neoself/views.py
@@ -235,7 +235,7 @@ class ReflectionHabitView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         test = self.kwargs['id']
         dot = Questionnaire.objects.get(id=test)
-        serializer.save(questionnaire=dot,name=dot.habit_name, metric_label=dot.metric_label, goal_metric=dot.goal_metric)
+        serializer.save(questionnaire=dot, metric_baseline=dot.metric_baseline, metric_label=dot.metric_label, goal_metric=dot.goal_metric)
 
 class ReflectionLatestHabitView(generics.ListAPIView):
     queryset = Reflection.objects.all()
